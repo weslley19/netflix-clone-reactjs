e11ce158de0aa9ba0cf72fac80a7f453297950e9
weslley19 <weslleyoliveira258@gmail.com>
2020-12-08 18:08:17 -0300

    feat: sort banner background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   // Ao carregar a paǵina roda o que tem dentro
   useEffect(() => {
     const loadAll = async () => {
+
+      // Pegando lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o featured
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * originals[0].items.results.length -1);
+      let chosen = originals[0].items.results[randomChosen];
+
+      console.log(chosen);
     }
 
     loadAll();
@@ -18,6 +30,11 @@ export default () => {
 
   return (
     <div className="page">
+
+      { featuredData &&
+        <FeaturedMovie item={ featuredData } />
+      }
+
       <section className="lists">
         { movieList.map((item, key) => (
           <MovieRow key={ key } title={ item.title } items={ item.items } />
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './MovieRow.css';
 
 export default ({ title, items }) => {
-  return (
-    <div>
-      <h2>{ title }</h2>
-      <div className="movieRow--lisarea">
-        { items.results.length > 0 && items.results.map((item, key) => (
-          <img src={ `https://image.tmdb.org/t/p/w300${item.poster_path}` } />
-         )) }
-      </div>
-    </div>
-  );
+    return (
+        <div className="movieRow">
+            <h2>{title}</h2>
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={ key } className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
