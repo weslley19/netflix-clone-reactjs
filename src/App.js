e1526f865fb1a05734bf52922f443101976fdf7f
weslley19 <weslleyoliveira258@gmail.com>
2020-12-09 05:42:35 -0300

    feat: add navigation styles

--- src/App.js
+++ src/App.js
@@ -62,6 +62,12 @@ export default () => {
           <MovieRow key={ key } title={ item.title } items={ item.items } />
         )) }
       </section>
+
+      <footer>
+        Feito por Weslley Oliveira <br />
+        Direitos de imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 }
